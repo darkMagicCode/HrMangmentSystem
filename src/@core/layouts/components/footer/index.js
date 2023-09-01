cf9a5e8ce9c90fefb6bf6c6c4b93f26a39d1fd5f
darkMagicCode <darkmagiccode@gmail.com>
2023-09-01 18:39:13 +0300

    a

--- src/@core/layouts/components/footer/index.js
+++ src/@core/layouts/components/footer/index.js
@@ -2,20 +2,20 @@
 import { Heart } from 'react-feather'
 
 const Footer = () => {
-  return (
-    <p className='clearfix mb-0'>
-      <span className='float-md-start d-block d-md-inline-block mt-25'>
-        COPYRIGHT © {new Date().getFullYear()}{' '}
-        <a href='https://1.envato.market/pixinvent_portfolio' target='_blank' rel='noopener noreferrer'>
-          Pixinvent
-        </a>
-        <span className='d-none d-sm-inline-block'>, All rights Reserved</span>
-      </span>
-      <span className='float-md-end d-none d-md-block'>
-        Hand-crafted & Made with
-        <Heart size={14} />
-      </span>
-    </p>
+  return (<></>
+    // <p className='clearfix mb-0'>
+    //   <span className='float-md-start d-block d-md-inline-block mt-25'>
+    //     COPYRIGHT © {new Date().getFullYear()}{' '}
+    //     <a href='' target='_blank' rel='noopener noreferrer'>
+    //       Dot
+    //     </a>
+    //     <span className='d-none d-sm-inline-block'>, All rights Reserved</span>
+    //   </span>
+    //   <span className='float-md-end d-none d-md-block'>
+    //     Manage your HR System
+    //     <Heart size={14} />
+    //   </span>
+    // </p>
   )
 }
 
