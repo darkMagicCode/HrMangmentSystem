8132b63210dac64810407b98bbf96b9d777363b2
darkMagicCode <darkmagiccode@gmail.com>
2023-09-01 22:49:46 +0300

    ش

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import React, { Suspense, useEffect } from 'react'
 import Router from './router/Router'
 
 const App = () => {
-  useEffect(() => {
-    // Calculate the desired zoom level (90%)
-    const desiredZoom = 0.9;
+  // useEffect(() => {
+  //   // Calculate the desired zoom level (90%)
+  //   const desiredZoom = 0.9;
     
-    // Calculate the current device pixel ratio
-    const currentPixelRatio = window.devicePixelRatio;
+  //   // Calculate the current device pixel ratio
+  //   const currentPixelRatio = window.devicePixelRatio;
     
-    // Calculate the required zoom to achieve the desired zoom level
-    const zoom = desiredZoom / currentPixelRatio;
+  //   // Calculate the required zoom to achieve the desired zoom level
+  //   const zoom = desiredZoom / currentPixelRatio;
 
-    // Apply the zoom to the body element
-    document.body.style.zoom = zoom.toString();
+  //   // Apply the zoom to the body element
+  //   document.body.style.zoom = zoom.toString();
     
-    // Cleanup by resetting the zoom when the component unmounts
+  //   // Cleanup by resetting the zoom when the component unmounts
   
-  }, []);
+  // }, []);
   return (
     <Suspense fallback={null}>
       <Router />
