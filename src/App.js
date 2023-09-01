0b06c98e3593d41c14785cebba281b1c44db6a70
darkMagicCode <darkmagiccode@gmail.com>
2023-09-01 22:28:35 +0300

    a

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import Router from './router/Router'
 
 const App = () => {
   useEffect(() => {
-    // Set the zoom level to 90% when the component mounts
-    document.body.style.zoom = '90%';
- 
+    // Calculate the desired zoom level (90%)
+    const desiredZoom = 0.8;
+    
+    // Calculate the current device pixel ratio
+    const currentPixelRatio = window.devicePixelRatio;
+    
+    // Calculate the required zoom to achieve the desired zoom level
+    const zoom = desiredZoom / currentPixelRatio;
+
+    // Apply the zoom to the body element
+    document.body.style.zoom = zoom.toString();
+    
+    // Cleanup by resetting the zoom when the component unmounts
+  
   }, []);
   return (
     <Suspense fallback={null}>
